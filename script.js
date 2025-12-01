853a056abb23e7263ca04b9c1642dbd2c204634a
Dinh <dpn.toe@gmail.com>
2025-11-30 23:24:58 -0800

    added About me section with floating skils

--- script.js
+++ script.js
@@ -13,4 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+  // SIMPLE FADE-IN ON SCROLL
+  const sections = document.querySelectorAll("section");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  sections.forEach((sec) => observer.observe(sec));
+
+  // Reveal About section on scroll for experiment purposes for future use
+  const aboutSection = document.querySelector("#about");
+
+  const aboutObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          aboutSection.classList.add("visible");
+          aboutObserver.unobserve(aboutSection);
+        }
+      });
+    },
+    {
+      threshold: 0.2,
+    }
+  );
+
+  aboutObserver.observe(aboutSection);
 });
